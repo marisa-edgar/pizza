4b99649052a5e29a82d8142a01a9e75c8426c9b4
marisa-edgar <marisaedgar1212@gmail.com>
2021-11-09 16:36:09 -0800

    fix if statements

--- js/scripts.js
+++ js/scripts.js
@@ -16,70 +16,25 @@ Pizza.prototype.modifyCost = function() {
     this.price;
   }
 
-  if (this.top1 === "onion") {
+  //topping 1 
+  if (this.top1 === "Onion") {
     this.price += 1;
-  } else if (this.top1 === "mushroom") {
+  }
+   if (this.top2 === "Mushroom") {
     this.price += 1;
-  } else if (this.top1 === "black olive") {
+  }
+   if (this.top3 === "Black Olive") {
     this.price += 1;
-  } else if (this.top1 === "bell pepper") {
+  }
+   if (this.top4 === "Bell Pepper") {
     this.price += 1;
-  } else if (this.top1 === "jalapeno") {
+  }
+   if (this.top5 === "Jalapeno") {
     this.price += 1;
   }
   
-
-  // next topping 2
-  if (this.top2 === "onion") {
-    this.price += 1;
-  } else if (this.top2 === "mushroom") {
-    this.price += 1;
-  } else if (this.top2 === "black olive") {
-    this.price += 1;
-  } else if (this.top2 === "bell pepper") {
-    this.price += 1;
-  } else if (this.top2 === "jalapeno") {
-    this.price += 1;
-  }
-  // next topping 3
-  if (this.top3 === "onion") {
-    this.price += 1;
-  } else if (this.top3 === "mushroom") {
-    this.price += 1;
-  } else if (this.top3 === "black olive") {
-    this.price += 1;
-  } else if (this.top3 === "bell pepper") {
-    this.price += 1;
-  } else if (this.top3 === "jalapeno") {
-    this.price += 1;
-  }
-
-  // next topping 4
-  if (this.top4 === "onion") {
-    this.price += 1;
-  } else if (this.top4 === "mushroom") {
-    this.price += 1;
-  } else if (this.top4 === "black olive") {
-    this.price += 1;
-  } else if (this.top4 === "bell pepper") {
-    this.price += 1;
-  } else if (this.top4 === "jalapeno") {
-    this.price += 1;
-  }
-  // next topping 5
-  if (this.top5 === "onion") {
-    this.price += 1;
-  } else if (this.top5 === "mushroom") {
-    this.price += 1;
-  } else if (this.top5 === "black olive") {
-    this.price += 1;
-  } else if (this.top5 === "bell pepper") {
-    this.price += 1;
-  } else if (this.top5 === "jalapeno") {
-    this.price += 1;
-  }
 };
-//console.log(Pizza.price)
+
 function showTopAndSize(pizza1) {
   $("#pizza-price").show();
   $(".pizza-size").html(pizza1.size);
@@ -94,17 +49,12 @@ $(document).ready(function(){
   $("form#pizza-select").submit(function(event) {
     event.preventDefault();
     let pizzaSize =$("input:radio[name=size]:checked").val();
-    let pizzaTop1 = $("input:radio[name=top1]:checked").val();
-    let pizzaTop2 = $("input:radio[name=top2]:checked").val();
-    let pizzaTop3 = $("input:radio[name=top3]:checked").val();
-    let pizzaTop4 = $("input:radio[name=top4]:checked").val();
-    let pizzaTop5 = $("input:radio[name=top5]:checked").val();
-    $("input:radio[name=size]:checked").val("");
-    $("input:radio[name=topping1]:checked").val("");
-    $("input:radio[name=topping2]:checked").val("");
-    $("input:radio[name=topping3]:checked").val("");
-    $("input:radio[name=topping4]:checked").val("");
-    $("input:radio[name=topping5]:checked").val("");
+    let pizzaTop1 = $("input:checkbox[name=topping1]:checked").val();
+    let pizzaTop2 = $("input:checkbox[name=topping2]:checked").val();
+    let pizzaTop3 = $("input:checkbox[name=topping3]:checked").val();
+    let pizzaTop4 = $("input:checkbox[name=topping4]:checked").val();
+    let pizzaTop5 = $("input:checkbox[name=topping5]:checked").val();
+    console.log(pizzaTop1,pizzaTop2,pizzaTop3,pizzaTop4,pizzaTop5)
     let pizza1 = new Pizza(pizzaSize,pizzaTop1, pizzaTop2, pizzaTop3, pizzaTop4, pizzaTop5);
     pizza1.modifyCost();
     showTopAndSize(pizza1);
